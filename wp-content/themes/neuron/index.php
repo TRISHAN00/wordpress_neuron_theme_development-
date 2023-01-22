a2212f76ed96abd1406c9f1bdd52af12a8ff470d
TRISHAN00 <trishansaha43@gmail.com>
2023-01-22 12:50:10 +0600

    add header menu without dropdown

--- wp-content/themes/neuron/index.php
+++ wp-content/themes/neuron/index.php
@@ -102,11 +102,11 @@
 	                <!-- Collect the nav links, forms, and other content for toggling -->
 	                <div class="collapse navbar-collapse" id="navbar-menu">
 						<?php
-							$args = array(
-								'theme_location' => 'menu-1',
+							wp_nav_menu( array(
+								'theme-location' => 'menu-1',
 								'menu_class' => 'nav navbar-nav navbar-right',
-							);
-							wp_nav_menu($args)
+								
+							) )
 						?>
 	                </div><!-- /.navbar-collapse -->
 	            </div>
